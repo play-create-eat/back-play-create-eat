decd0beca31da78f6e5022ff5c46a7025b04e765
Ispas Eugeniu <eugenispas15@gmail.com>
2025-01-15 17:36:17 +0200

    docs(pandadoc): create document
    update docs

--- app/Http/Controllers/Api/v1/PandaDocController.php
+++ app/Http/Controllers/Api/v1/PandaDocController.php
@@ -16,70 +16,70 @@ class PandaDocController extends Controller
 
 
     /**
-     * @OA\Post(
-     *     path="/api/v1/documents",
-     *     summary="Create a document from template and prepare it for signing.",
-     *     tags={"Documents"},
-     *     @OA\RequestBody(
-     *         required=true,
-     *         @OA\JsonContent(
-     *             @OA\Property(property="FIRST_NAME", type="string", example="John", description="Recipient's first name"),
-     *             @OA\Property(property="LAST_NAME", type="string", example="Doe", description="Recipient's last name"),
-     *             @OA\Property(property="PHONE_NUMBER", type="string", example="+971501234567", description="Recipient's phone number"),
-     *             @OA\Property(property="EMAIL_ADDRESS", type="string", example="tech@playcreateeat.ae", description="Recipient's email address"),
-     *             @OA\Property(property="PASSPORT_NUMBER", type="string", example="P1234567", description="Recipient's passport number"),
-     *             @OA\Property(property="KIDS_ARRAY", type="string", example="Alex Malii from 30.05.2004", description="Additional information about kids.")
-     *         )
-     *     ),
-     *     @OA\Response(
-     *         response=200,
-     *         description="Document created and ready for signing.",
-     *         @OA\JsonContent(
-     *             @OA\Property(property="message", type="string", example="Document is ready for signing."),
-     *             @OA\Property(property="document_id", type="string", example="xyz789", description="The ID of the newly created document."),
-     *             @OA\Property(property="signing_link", type="string", example="https://sign.pandadoc.com/signinglink", description="The link to sign the document.")
-     *         )
-     *     ),
-     *     @OA\Response(
-     *         response=500,
-     *         description="Document creation or signing preparation failed.",
-     *         @OA\JsonContent(
-     *             @OA\Property(property="error", type="string", example="Document is not ready for signing.")
-     *         )
-     *     )
-     * )
-     */
-    public function create()
-    {
-        $recipientData = [
-            "FIRST_NAME"      => ["value" => "John"],
-            "LAST_NAME"       => ["value" => "Doe"],
-            "PHONE_NUMBER"    => ["value" => "+971501234567"],
-            "EMAIL_ADDRESS"   => ["value" => "tech@playcreateeat.ae"],
-            "PASSPORT_NUMBER" => ["value" => "P1234567"],
-        ];
+ * @OA\Post(
+ *     path="/api/v1/documents",
+ *     summary="Create a document from template and prepare it for signing.",
+ *     tags={"Documents"},
+ *     @OA\RequestBody(
+ *         required=true,
+ *         @OA\JsonContent(
+ *             @OA\Property(property="FIRST_NAME", type="string", example="John", description="Recipient's first name"),
+ *             @OA\Property(property="LAST_NAME", type="string", example="Doe", description="Recipient's last name"),
+ *             @OA\Property(property="PHONE_NUMBER", type="string", example="+971501234567", description="Recipient's phone number"),
+ *             @OA\Property(property="EMAIL_ADDRESS", type="string", example="tech@playcreateeat.ae", description="Recipient's email address"),
+ *             @OA\Property(property="PASSPORT_NUMBER", type="string", example="P1234567", description="Recipient's passport number"),
+ *             @OA\Property(property="KIDS_ARRAY", type="string", example="Alex Malii from 30.05.2004", description="Additional information about kids.")
+ *         )
+ *     ),
+ *     @OA\Response(
+ *         response=200,
+ *         description="Document created and ready for signing.",
+ *         @OA\JsonContent(
+ *             @OA\Property(property="message", type="string", example="Document is ready for signing."),
+ *             @OA\Property(property="document_id", type="string", example="xyz789", description="The ID of the newly created document."),
+ *             @OA\Property(property="signing_link", type="string", example="https://sign.pandadoc.com/signinglink", description="The link to sign the document.")
+ *         )
+ *     ),
+ *     @OA\Response(
+ *         response=500,
+ *         description="Document creation or signing preparation failed.",
+ *         @OA\JsonContent(
+ *             @OA\Property(property="error", type="string", example="Document is not ready for signing.")
+ *         )
+ *     )
+ * )
+ */
+public function create()
+{
+    $recipientData = [
+        "FIRST_NAME"      => ["value" => "John"],
+        "LAST_NAME"       => ["value" => "Doe"],
+        "PHONE_NUMBER"    => ["value" => "+971501234567"],
+        "EMAIL_ADDRESS"   => ["value" => "tech@playcreateeat.ae"],
+        "PASSPORT_NUMBER" => ["value" => "P1234567"],
+    ];
 
-        $recipientData = [...$recipientData, 'KIDS_ARRAY' => ['value' => "Alex Malii from 30.05.2004"]];
+    $recipientData = [...$recipientData, 'KIDS_ARRAY' => ['value' => "Alex Malii from 30.05.2004"]];
 
-        try {
-            $document = $this->pandadoc->createDocumentFromTemplate("WVGshXQkryavX2rVoCwQf5", $recipientData);
+    try {
+        $document = $this->pandadoc->createDocumentFromTemplate("WVGshXQkryavX2rVoCwQf5", $recipientData);
 
-            if ($document->getId()) {
-                if ($this->pandadoc->checkDocumentStatus($document->getId())) {
-                    $this->pandadoc->sendDocument($document->getId());
-                    $response = $this->pandadoc->generateDocumentLink($document->getId());
-                    $signingResponse = json_decode($response->getContent(), true);
+        if ($document->getId()) {
+            if ($this->pandadoc->checkDocumentStatus($document->getId())) {
+                $this->pandadoc->sendDocument($document->getId());
+                $response = $this->pandadoc->generateDocumentLink($document->getId());
+                $signingResponse = json_decode($response->getContent(), true);
 
-                    return response()->json([...$signingResponse, 'document_id' => $document->getId()]);
-                }
-
-                return response()->json(['error' => 'Document is not ready for signing.'], 500);
+                return response()->json([...$signingResponse, 'document_id' => $document->getId()]);
             }
-            return response()->json(['error' => 'Document creation failed.'], 500);
-        } catch (Exception $e) {
-            return response()->json(['error' => $e->getMessage()], 500);
+
+            return response()->json(['error' => 'Document is not ready for signing.'], 500);
         }
+        return response()->json(['error' => 'Document creation failed.'], 500);
+    } catch (Exception $e) {
+        return response()->json(['error' => $e->getMessage()], 500);
     }
+}
 
     /**
      * @OA\Get(
