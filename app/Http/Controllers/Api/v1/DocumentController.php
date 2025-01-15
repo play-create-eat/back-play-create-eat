7ffcae99efbed6f7c01618441bd1dfdc887e2816
Ispas Eugeniu <42368950+DiplNext@users.noreply.github.com>
2025-01-15 10:10:51 +0200

    Docs/document (#16)
    * docs(document): create document
    
    fix swagger docs for document create endpoint
    
    * chore(document): remove unused
    
    remove unused method

--- app/Http/Controllers/Api/v1/DocumentController.php
+++ app/Http/Controllers/Api/v1/DocumentController.php
@@ -7,22 +7,4 @@ use Illuminate\Http\Request;
 
 class DocumentController extends Controller
 {
-    /**
-     * @OA\Post(
-     *     path="/api/v1/documents",
-     *     summary="Sign a document",
-     *     tags={"Documents"},
-     *     @OA\Response(
-     *         response=200,
-     *         description="Document signed successfully",
-     *         @OA\JsonContent(
-     *             @OA\Property(property="message", type="string", example="Document signed successfully.")
-     *         )
-     *     )
-     * )
-     */
-    public function store()
-    {
-        return response()->json(['message' => 'Document signed successfully.']);
-    }
 }
