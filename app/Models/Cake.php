09657bd86298ea50b9644c4659320e555c789bb6
Ispas Eugeniu <eugenispas15@gmail.com>
2025-03-10 22:22:38 +0200

    feat(cake): add image to cake
    add image to cake

--- app/Models/Cake.php
+++ app/Models/Cake.php
@@ -3,16 +3,28 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Cake extends Model
+class Cake extends Model implements HasMedia
 {
+    use InteractsWithMedia;
+
+    protected $appends = ['image'];
+
     protected $fillable = [
         'type',
         'price_per_kg',
     ];
 
+    public function getImageAttribute(): ?string
+    {
+        return $this->getFirstMediaUrl('cake_images') ?: null;
+    }
+
+
     public function registerMediaCollections(): void
     {
-        $this->addMediaCollection('cake_images')->useDisk('public');
+        $this->addMediaCollection('cake_images')->useDisk('s3');
     }
 }
