5f1d34afb06cb501108c43018d1b7e852d73c65f
Ispas Eugeniu <eugenispas15@gmail.com>
2025-03-18 18:59:45 +0200

    fix(migrations): wallet migrations
    fix wallet migrations

--- database/migrations/2024_12_14_094816_create_families_table.php
+++ database/migrations/2024_12_14_094816_create_families_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -14,6 +13,9 @@ return new class extends Migration
         Schema::create('families', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('stripe_customer_id')
+                ->nullable()
+                ->unique();
             $table->timestamps();
             $table->softDeletes();
         });
