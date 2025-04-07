9e5f7eecf50a8c70aa5435e7d025d92ffa2cc5f5
Vitaly Rotari <cky.adc@gmail.com>
2025-04-07 23:46:43 +0300

    add date param for products resource (#124)

--- app/Http/Resources/Api/v1/ProductResource.php
+++ app/Http/Resources/Api/v1/ProductResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\Api\v1;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -17,11 +18,16 @@ class ProductResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        if ($date = $request->input('date')) {
+          $date = Carbon::parse($date);
+        }
+
         return [
             'id'            => $this->id,
             'name'          => $this->name,
             'description'   => $this->description,
-            'price'         => $this->price,
+            'price'         => $this->getPriceByDate($date),
+            'price_base'    => $this->price,
             'price_weekend' => $this->price_weekend,
             'fee_percent'   => $this->fee_percent,
             'is_extendable' => $this->is_extendable,
