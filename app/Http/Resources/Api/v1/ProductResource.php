025769e2e0f8742e14fc9bbf606c9ac1d8daef7a
Vitaly Rotari <cky.adc@gmail.com>
2025-05-26 22:58:02 +0300

    add discount prices

--- app/Http/Resources/Api/v1/ProductResource.php
+++ app/Http/Resources/Api/v1/ProductResource.php
@@ -23,17 +23,19 @@ class ProductResource extends JsonResource
         }
 
         return [
-            'id'                => $this->id,
-            'name'              => $this->name,
-            'description'       => $this->description,
-            'price'             => $this->getFinalPrice($date),
-            'price_base'        => $this->price,
-            'price_weekend'     => $this->price_weekend,
-            'discount_percent'  => (double)$this->discount_percent,
-            'cashback_percent'  => (double)$this->cashback_percent,
-            'fee_percent'       => (double)$this->fee_percent,
-            'is_extendable'     => $this->is_extendable,
-            'features'          => ProductFeatureResource::collection($this->whenLoaded('features')),
+            'id'                        => $this->id,
+            'name'                      => $this->name,
+            'description'               => $this->description,
+            'price'                     => $this->getFinalPrice($date),
+            'price_base'                => $this->price,
+            'price_weekend'             => $this->price_weekend,
+            'discount_price_weekday'    => $this->discount_price_weekday,
+            'discount_price_weekend'    => $this->discount_price_weekend,
+            'discount_percent'          => (double)$this->discount_percent,
+            'cashback_percent'          => (double)$this->cashback_percent,
+            'fee_percent'               => (double)$this->fee_percent,
+            'is_extendable'             => $this->is_extendable,
+            'features'                  => ProductFeatureResource::collection($this->whenLoaded('features')),
         ];
     }
 }
